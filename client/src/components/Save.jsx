ddcbb46b35496cf7720310d64b166b6b28187c6c
Arnold Nillas <125740105+arnoldzkie2@users.noreply.github.com>
2023-04-15 17:56:06 +0800

    update

--- client/src/components/Save.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-//this is just a save modal when user save their edited notes.
-const Save = () => {
-  return <div className="save-modal">Saved</div>;
-};
-
-export default Save;
