d342bab1a6344fb803fb8db931fdc1a872283c46
Arnold Nillas <125740105+arnoldzkie2@users.noreply.github.com>
2023-04-18 18:04:17 +0800

    updated

--- src/routes/noted.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { getAllNotes, getNote, deleteNote, updateNote, createNote } from '../controller/noted.js';
-const notes = express.Router();
-
-notes.route('/:id').get(getAllNotes).post(createNote)
-notes.route('/:id/:noteID').get(getNote).delete(deleteNote).patch(updateNote)
-export { notes }
--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {renderUser, registerUser , changePasword, loginUser, getAllUser} from '../controller/user.js'
-const user = express.Router()
-
-user.route('/register').post(registerUser)
-user.route('/users').get(getAllUser)
-user.route('/login').post(loginUser)
-user.route('/:id').get(renderUser).patch(changePasword)
-export { user }
